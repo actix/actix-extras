a60c530fd5d0e9fb5de9f151f043fe73bb6ff610
Luca Palmieri <lpalmieri@truelayer.com>
2020-09-27 16:27:51 +0100

    First release.

---
+++ README.md
@@ -0,0 +1,80 @@
+<h1 align="center">tracing-actix-web</h1>
+<div align="center">
+ <strong>
+   Structured logging for actix-web applications.
+ </strong>
+</div>
+
+`tracing-actix-web` provides [`TracingLogger`], a middleware to log request and response info when using the [`actix-web`] framework.
+
+[`TracingLogger`] is designed as a drop-in replacement of [`actix-web`]'s [`Logger`].
+
+[`Logger`] is built on top of the [`log`] crate: you need to use regular expressions to parse the request information out of the logged message.
+
+[`TracingLogger`] relies on [`tracing`], a modern instrumentation framework for structured logging: all request information is captured as a machine-parsable set of key-value pairs.  
+It also enables propagation of context information to children spans.
+
+## How to install
+
+Add `tracing-actix-web` to your dependencies:
+```toml
+[dependencies]
+# ...
+tracing-actix-web = "0.1"
+```
+If you are using [`cargo-edit`](https://github.com/killercup/cargo-edit), run
+```bash
+cargo add tracing-actix-web
+```
+
+## Usage example
+
+Register `TracingLogger` as a middleware for your application using `.wrap` on `App`.  
+Add a `Subscriber` implementation to output logs to the console.
+
+```rust
+use actix_web::middleware::Logger;
+use actix_web::App;
+use tracing::{Subscriber, subscriber::set_global_default};
+use tracing_actix_web::TracingLogger;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+
+/// Compose multiple layers into a `tracing`'s subscriber.
+pub fn get_subscriber(
+    name: String,
+    env_filter: String
+) -> impl Subscriber + Send + Sync {
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or(EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(
+        name.into(),
+        std::io::stdout
+    );
+    Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
+}
+
+/// Register a subscriber as global default to process span data.
+///
+/// It should only be called once!
+pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+    LogTracer::init().expect("Failed to set logger");
+    set_global_default(subscriber).expect("Failed to set subscriber");
+}
+
+fn main() {
+    let subscriber = get_subscriber("app".into(), "info".into());
+    init_subscriber(subscriber);
+
+    let app = App::new().wrap(TracingLogger);
+}
+```
+
+[`TracingLogger`]: https://docs.rs/tracing-actix-web/0.1.0/tracing-actix-web/#struct.TracingLogger.html
+[`actix-web`]: https://docs.rs/actix-web
+[`Logger`]: https://docs.rs/actix-web/2.0.0/actix_web/middleware/struct.Logger.html
+[`log`]: https://docs.rs/log
+[`tracing`]: https://docs.rs/tracing
---
+++ src/lib.rs
@@ -0,0 +1,155 @@
+//! `tracing-actix-web` provides [`TracingLogger`], a middleware to log request and response info
+//! when using the [`actix-web`] framework.
+//!
+//! [`TracingLogger`] is designed as a drop-in replacement of [`actix-web`]'s [`Logger`].
+//!
+//! [`Logger`] is built on top of the [`log`] crate: you need to use regular expressions to parse
+//! the request information out of the logged message.
+//!
+//! [`TracingLogger`] relies on [`tracing`], a modern instrumentation framework for structured
+//! logging: all request information is captured as a machine-parsable set of key-value pairs.
+//! It also enables propagation of context information to children spans.
+//!
+//! [`TracingLogger`]: #struct.TracingLogger.html
+//! [`actix-web`]: https://docs.rs/actix-web
+//! [`Logger`]: https://docs.rs/actix-web/2.0.0/actix_web/middleware/struct.Logger.html
+//! [`log`]: https://docs.rs/log
+//! [`tracing`]: https://docs.rs/tracing
+use actix_web::dev::{Service, ServiceRequest, ServiceResponse, Transform};
+use actix_web::Error;
+use futures::future::{ok, Ready};
+use futures::task::{Context, Poll};
+use std::future::Future;
+use std::pin::Pin;
+use tracing::Span;
+use tracing_futures::Instrument;
+
+/// `TracingLogger` is a middleware to log request and response info in a structured format.
+///
+/// `TracingLogger` is designed as a drop-in replacement of [`actix-web`]'s [`Logger`].
+///
+/// [`Logger`] is built on top of the [`log`] crate: you need to use regular expressions to parse
+/// the request information out of the logged message.
+///
+/// `TracingLogger` relies on [`tracing`], a modern instrumentation framework for structured
+/// logging: all request information is captured as a machine-parsable set of key-value pairs.
+/// It also enables propagation of context information to children spans.
+///
+/// ## Usage
+///
+/// Register `TracingLogger` as a middleware for your application using `.wrap` on `App`.  
+/// Add a `Subscriber` implementation to output logs to the console.
+///
+/// ```rust
+/// use actix_web::middleware::Logger;
+/// use actix_web::App;
+/// use tracing::{Subscriber, subscriber::set_global_default};
+/// use tracing_actix_web::TracingLogger;
+/// use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+/// use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+///
+/// /// Compose multiple layers into a `tracing`'s subscriber.
+/// pub fn get_subscriber(
+///     name: String,
+///     env_filter: String
+/// ) -> impl Subscriber + Send + Sync {
+///     let env_filter = EnvFilter::try_from_default_env()
+///         .unwrap_or(EnvFilter::new(env_filter));
+///     let formatting_layer = BunyanFormattingLayer::new(
+///         name.into(),
+///         std::io::stdout
+///     );
+///     Registry::default()
+///         .with(env_filter)
+///         .with(JsonStorageLayer)
+///         .with(formatting_layer)
+/// }
+///
+/// /// Register a subscriber as global default to process span data.
+/// ///
+/// /// It should only be called once!
+/// pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+///     LogTracer::init().expect("Failed to set logger");
+///     set_global_default(subscriber).expect("Failed to set subscriber");
+/// }
+///
+/// fn main() {
+///     let subscriber = get_subscriber("app".into(), "info".into());
+///     init_subscriber(subscriber);
+///
+///     let app = App::new().wrap(TracingLogger);
+/// }
+/// ```
+///
+/// [`actix-web`]: https://docs.rs/actix-web
+/// [`Logger`]: https://docs.rs/actix-web/2.0.0/actix_web/middleware/struct.Logger.html
+/// [`log`]: https://docs.rs/log
+/// [`tracing`]: https://docs.rs/tracing
+pub struct TracingLogger;
+
+impl<S, B> Transform<S> for TracingLogger
+where
+    S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Request = ServiceRequest;
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Transform = TracingLoggerMiddleware<S>;
+    type InitError = ();
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ok(TracingLoggerMiddleware { service })
+    }
+}
+
+#[doc(hidden)]
+pub struct TracingLoggerMiddleware<S> {
+    service: S,
+}
+
+impl<S, B> Service for TracingLoggerMiddleware<S>
+where
+    S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Request = ServiceRequest;
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.service.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: ServiceRequest) -> Self::Future {
+        let user_agent = req
+            .headers()
+            .get("User-Agent")
+            .map(|h| h.to_str().unwrap_or(""))
+            .unwrap_or("");
+        let span = tracing::info_span!(
+            "Request",
+            request_path = %req.path(),
+            user_agent = %user_agent,
+            client_ip_address = %req.connection_info().remote().unwrap_or(""),
+            status_code = tracing::field::Empty,
+        );
+        let fut = self.service.call(req);
+        Box::pin(
+            async move {
+                let outcome = fut.await;
+                let status_code = match &outcome {
+                    Ok(response) => response.response().status(),
+                    Err(error) => error.as_response_error().status_code(),
+                };
+                Span::current().record("status_code", &status_code.as_u16());
+                outcome
+            }
+            .instrument(span),
+        )
+    }
+}
