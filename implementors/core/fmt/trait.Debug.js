34f9e0ec7f721402c62150bfed16a4b1157daada
robjtede <robjtede@icloud.com>
2020-10-10 13:27:58 +0000

    Deploying to gh-pages from  @ 06f17ec2234a0f9b631c41ce2a501c7cd56f6260 🚀

--- implementors/core/fmt/trait.Debug.js
+++ implementors/core/fmt/trait.Debug.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["actix_redis"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"actix_redis/struct.Command.html\" title=\"struct actix_redis::Command\">Command</a>","synthetic":false,"types":["actix_redis::redis::Command"]},{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"actix_redis/enum.Error.html\" title=\"enum actix_redis::Error\">Error</a>","synthetic":false,"types":["actix_redis::Error"]}];
+implementors["actix_redis"] = [{"text":"impl Debug for Command","synthetic":false,"types":[]},{"text":"impl Debug for Error","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
