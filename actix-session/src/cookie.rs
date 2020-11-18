fff0b682f11f62d5ebb99f048e547c7e0b3ffa93
Quentin Kniep <hello@quentinkniep.com>
2020-11-18 17:45:39 +0100

    fix purge from other paths than root

--- actix-session/src/cookie.rs
+++ actix-session/src/cookie.rs
@@ -136,6 +136,7 @@ impl CookieSessionInner {
     /// invalidates session cookie
     fn remove_cookie<B>(&self, res: &mut ServiceResponse<B>) -> Result<(), Error> {
         let mut cookie = Cookie::named(self.name.clone());
+        cookie.set_path(self.path.clone());
         cookie.set_value("");
         cookie.set_max_age(Duration::zero());
         cookie.set_expires(OffsetDateTime::now_utc() - Duration::days(365));
