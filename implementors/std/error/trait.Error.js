1fe051c7c03d5b9be9f8a62989ffb913d61182e9
JohnTitor <huyuumi.dev@gmail.com>
2020-11-30 11:22:09 +0000

    Deploying to gh-pages from  @ a22e4cfa36de349a57c3ac4d924c69c9805a945b 🚀

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["actix_redis"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
