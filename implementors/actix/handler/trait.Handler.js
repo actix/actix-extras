2ec31329cc9fd9af3ad70ee2d3da510df3177af1
robjtede <robjtede@icloud.com>
2021-03-22 05:09:45 +0000

    Deploying to gh-pages from  @ b0854ed1441fcc9e5eb38da42fda0380f8ad34e8 🚀

--- implementors/actix/handler/trait.Handler.js
+++ implementors/actix/handler/trait.Handler.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["actix_redis"] = [{"text":"impl Debug for Command","synthetic":false,"types":[]},{"text":"impl Debug for Error","synthetic":false,"types":[]}];
+implementors["actix_redis"] = [{"text":"impl Handler&lt;Command&gt; for RedisActor","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
