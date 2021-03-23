afa560c27a5812f294f31b6ca5a6bca89efa7a8c
robjtede <robjtede@icloud.com>
2021-03-23 22:38:11 +0000

    Deploying to gh-pages from  @ fc6563a019ce9473da3e5af5b1f03a52502d9c6f 🚀

--- actix_session/sidebar-items.js
+++ actix_session/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["SessionStatus",""]],"struct":[["CookieSession","Use cookies for session storage."],["Session","The high-level interface you use to modify session data."]],"trait":[["UserSession","Extraction of a [`Session`] object."]]});
+initSidebarItems({"enum":[["SessionStatus","Status of a [`Session`]."]],"struct":[["CookieSession","Use cookies for session storage."],["Session","The high-level interface you use to modify session data."]],"trait":[["UserSession","Extraction of a [`Session`] object."]]});
