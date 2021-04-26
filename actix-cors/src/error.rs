8ec38372e5bc8a6d23f5fa81d81f3f13186a8be8
yexiangyu <yexiangyu@gmail.com>
2021-04-26 14:32:07 +0800

    Update error.rs
    fix incompatible error_response implimentation for actix_web 4.0.0-beta.6

--- actix-cors/src/error.rs
+++ actix-cors/src/error.rs
@@ -1,4 +1,4 @@
-use actix_web::{http::StatusCode, HttpResponse, ResponseError};
+use actix_web::{body::Body, http::StatusCode, BaseHttpResponse, HttpResponse, ResponseError};
 
 use derive_more::{Display, Error};
 
@@ -50,7 +50,7 @@ impl ResponseError for CorsError {
         StatusCode::BAD_REQUEST
     }
 
-    fn error_response(&self) -> HttpResponse {
-        HttpResponse::with_body(StatusCode::BAD_REQUEST, self.to_string().into())
+    fn error_response(&self) -> BaseHttpResponse<Body> {
+        BaseHttpResponse::new(self.status_code()).set_body(Body::from(self.to_string()))
     }
 }
