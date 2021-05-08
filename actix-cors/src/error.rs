190f242ac083b5ee4632c9da111fde1ecd4f86bc
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-05-08 16:25:44 +0200

    Updated error.rs to fix the build error

--- actix-cors/src/error.rs
+++ actix-cors/src/error.rs
@@ -1,4 +1,4 @@
-use actix_web::{http::StatusCode, HttpResponse, ResponseError};
+use actix_web::{http::StatusCode, BaseHttpResponse, ResponseError};
 
 use derive_more::{Display, Error};
 
@@ -50,7 +50,7 @@ impl ResponseError for CorsError {
         StatusCode::BAD_REQUEST
     }
 
-    fn error_response(&self) -> HttpResponse {
-        HttpResponse::with_body(StatusCode::BAD_REQUEST, self.to_string().into())
+    fn error_response(&self) -> BaseHttpResponse {
+        BaseHttpResponse::with_body(StatusCode::BAD_REQUEST, self.to_string().into())
     }
 }
