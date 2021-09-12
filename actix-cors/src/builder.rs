da7ab7a3af865ac2684916b1b3a9bf4a6a1867ef
Ali MJ Al-Nasrawy <alimjalnasrawy@gmail.com>
2021-09-12 14:10:35 +0300

    cors: mke middleware generic over body type

--- actix-cors/src/builder.rs
+++ actix-cors/src/builder.rs
@@ -1,6 +1,9 @@
-use std::{collections::HashSet, convert::TryInto, iter::FromIterator, rc::Rc};
+use std::{
+    collections::HashSet, convert::TryInto, error::Error as StdError, iter::FromIterator, rc::Rc,
+};
 
 use actix_web::{
+    body::MessageBody,
     dev::{RequestHead, Service, ServiceRequest, ServiceResponse, Transform},
     error::{Error, Result},
     http::{self, header::HeaderName, Error as HttpError, HeaderValue, Method, Uri},
@@ -479,10 +482,12 @@ impl Default for Cors {
     }
 }
 
-impl<S> Transform<S, ServiceRequest> for Cors
+impl<S, B> Transform<S, ServiceRequest> for Cors
 where
-    S: Service<ServiceRequest, Response = ServiceResponse, Error = Error>,
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
     S::Future: 'static,
+    B: MessageBody + 'static,
+    B::Error: StdError,
 {
     type Response = ServiceResponse;
     type Error = Error;
