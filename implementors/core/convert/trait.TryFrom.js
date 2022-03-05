e8c9749828a627b9484a7d3c224b93c69bca7af5
robjtede <robjtede@icloud.com>
2022-03-05 23:25:11 +0000

    Deploying to gh-pages from  @ 7e6335a09fa21302cec29904543d707b5da867a3 🚀

---
+++ implementors/core/convert/trait.TryFrom.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["actix_session"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>&gt; for <a class=\"struct\" href=\"actix_session/storage/struct.SessionKey.html\" title=\"struct actix_session::storage::SessionKey\">SessionKey</a>","synthetic":false,"types":["actix_session::storage::session_key::SessionKey"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
