e251715dd92f1a1c635e93c1ac1cb7146113d833
robjtede <robjtede@icloud.com>
2022-07-03 20:20:50 +0000

    Deploying to gh-pages from  @ c2f068db668e660e2ab3e55793c9d493c078ff5c 🚀

--- source-files.js
+++ source-files.js
@@ -4,6 +4,6 @@ sourcesIndex["actix_identity"] = {"name":"","files":["cookie.rs","identity.rs","
 sourcesIndex["actix_limitation"] = {"name":"","files":["builder.rs","errors.rs","lib.rs","middleware.rs","status.rs"]};
 sourcesIndex["actix_protobuf"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["actix_redis"] = {"name":"","files":["lib.rs","redis.rs"]};
-sourcesIndex["actix_session"] = {"name":"","dirs":[{"name":"storage","files":["cookie.rs","interface.rs","mod.rs","redis_actor.rs","redis_rs.rs","session_key.rs","utils.rs"]}],"files":["lib.rs","middleware.rs","session.rs","session_ext.rs"]};
+sourcesIndex["actix_session"] = {"name":"","dirs":[{"name":"storage","files":["cookie.rs","interface.rs","mod.rs","redis_actor.rs","redis_rs.rs","session_key.rs","utils.rs"]}],"files":["config.rs","lib.rs","middleware.rs","session.rs","session_ext.rs"]};
 sourcesIndex["actix_web_httpauth"] = {"name":"","dirs":[{"name":"extractors","files":["basic.rs","bearer.rs","config.rs","errors.rs","mod.rs"]},{"name":"headers","dirs":[{"name":"authorization","dirs":[{"name":"scheme","files":["basic.rs","bearer.rs","mod.rs"]}],"files":["errors.rs","header.rs","mod.rs"]},{"name":"www_authenticate","dirs":[{"name":"challenge","dirs":[{"name":"bearer","files":["builder.rs","challenge.rs","errors.rs","mod.rs"]}],"files":["basic.rs","mod.rs"]}],"files":["header.rs","mod.rs"]}],"files":["mod.rs"]}],"files":["lib.rs","middleware.rs","utils.rs"]};
 createSourceSidebar();
