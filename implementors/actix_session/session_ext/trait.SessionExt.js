677c8562772ba7b56d9ab45ab6f3d3a519ae820a
robjtede <robjtede@icloud.com>
2022-08-10 08:16:25 +0000

    Deploying to gh-pages from  @ e61dbae860483fe37586d0ffe43dd8c5a42d9d55 🚀

--- implementors/actix_session/session_ext/trait.SessionExt.js
+++ implementors/actix_session/session_ext/trait.SessionExt.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["actix_session"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"actix_session":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
