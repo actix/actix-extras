eb3660a772c4b6da5a9f1e6bd3bf278559c844ed
Rob Ede <robjtede@icloud.com>
2022-09-11 21:11:33 +0100

    set same-site attribute when clearing session cookie (#284)
    fixes #282

--- actix-session/src/middleware.rs
+++ actix-session/src/middleware.rs
@@ -444,7 +444,8 @@ fn delete_session_cookie(
 ) -> Result<(), anyhow::Error> {
     let removal_cookie = Cookie::build(config.name.clone(), "")
         .path(config.path.clone())
-        .http_only(config.http_only);
+        .http_only(config.http_only)
+        .same_site(config.same_site);
 
     let mut removal_cookie = if let Some(ref domain) = config.domain {
         removal_cookie.domain(domain)
