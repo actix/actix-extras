fc50b08499f47d296110d6cda8e3a3974161d1a6
edgerunnergit <edgerunnergit@riseup.net>
2024-07-03 04:52:07 +0530

    make generate_session_key() public and change impl to use DistString

--- actix-session/src/storage/utils.rs
+++ actix-session/src/storage/utils.rs
@@ -1,17 +1,12 @@
-use rand::{distributions::Alphanumeric, rngs::OsRng, Rng as _};
-
 use crate::storage::SessionKey;
+use rand::distributions::Alphanumeric;
+use rand::distributions::DistString;
 
 /// Session key generation routine that follows [OWASP recommendations].
 ///
 /// [OWASP recommendations]: https://cheatsheetseries.owasp.org/cheatsheets/Session_Management_Cheat_Sheet.html#session-id-entropy
-pub(crate) fn generate_session_key() -> SessionKey {
-    let value = std::iter::repeat(())
-        .map(|()| OsRng.sample(Alphanumeric))
-        .take(64)
-        .collect::<Vec<_>>();
-
-    // These unwraps will never panic because pre-conditions are always verified
-    // (i.e. length and character set)
-    String::from_utf8(value).unwrap().try_into().unwrap()
+pub fn generate_session_key() -> SessionKey {
+    let session_key = Alphanumeric.sample_string(&mut rand::thread_rng(), 64);
+    // This unwrap should never panic because the String is guaranteed to be 64 alphanumeric characters
+    session_key.try_into().unwrap()
 }
