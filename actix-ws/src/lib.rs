95a97ede79e1a336b9292a4a9fc163498e1eeb8a
Yuki Okushi <huyuumi.dev@gmail.com>
2026-02-09 19:45:53 +0900

    docs(ws): fix example (#662)

--- actix-ws/src/lib.rs
+++ actix-ws/src/lib.rs
@@ -31,13 +31,12 @@ pub use self::{
 /// use std::io;
 /// use actix_web::{middleware::Logger, web, App, HttpRequest, HttpServer, Responder};
 /// use actix_ws::Message;
-/// use futures_util::StreamExt as _;
 ///
 /// async fn ws(req: HttpRequest, body: web::Payload) -> actix_web::Result<impl Responder> {
 ///     let (response, mut session, mut msg_stream) = actix_ws::handle(&req, body)?;
 ///
 ///     actix_web::rt::spawn(async move {
-///         while let Some(Ok(msg)) = msg_stream.next().await {
+///         while let Some(Ok(msg)) = msg_stream.recv().await {
 ///             match msg {
 ///                 Message::Ping(bytes) => {
 ///                     if session.pong(&bytes).await.is_err() {
